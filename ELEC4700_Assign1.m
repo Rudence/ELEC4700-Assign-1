390ca40e758c5314a845d1178dfd6c321b6edfaf
Rudi Hidvary <rudi.john.hidvary@gmail.com>
2020-01-29 10:27:57 -0500

    After lab Session

--- ELEC4700_Assign1.m
+++ ELEC4700_Assign1.m
@@ -5,7 +5,8 @@
 
 % QUESTIONS
 % Q1: how do i get the trajectories not just the current values 
-% Q2 mean free path for part 2 and how to get the time between collisions 
+% Q2: mean free path for part 2 and how to get the time between collisions 
+% Q3: How do i fix time on the temp graphs 
 
 clear
 clc
@@ -14,15 +15,15 @@ clc
 m0 = 9.11e-31; % electron mass (kg)
 k = 1.381e-23; % boltzmans constant 
 
-% Model Parameters
+%Model Parameters
 length = 200e-9;        % size of simulation in x dierection (m)
 height = 100e-9;        % size of simulation in y direction (m)
 temperature = 300;      % temperature in kelvin
 me = 0.26*m0;           % Effective mass of an electorn in our simulation
-e_num = 400;             % Number of electrons in the simulation 
-simlength = 200;        % Sests the number of iterations the simulation undergoes
+e_num = 300;             % Number of electrons in the simulation 
+simlength = 100;        % Sests the number of iterations the simulation undergoes
 graph_pause = 1;
-sim_pause = 0.00001;
+sim_pause = 0.001;
 
 
 % Initial Calculations 
@@ -30,7 +31,7 @@ sim_pause = 0.00001;
 thermal_velocity = sqrt((2*k*temperature)/me) % velocity in (m/s)
 
 % Initializing the Simulation Parameters 
-initial_xposition = length*rand(e_num,1) % Sets the initial x positions as a vector of randomly selected numbers over the length of the simulation
+initial_xposition = length*rand(e_num,1); % Sets the initial x positions as a vector of randomly selected numbers over the length of the simulation
 initial_yposition = height*rand(e_num,1); % Sets the initial y position as a vector of randomly selected numbers over the length of the simulation
 theta = 2*pi*rand(e_num,1); % Initializes a vector of random angles the size of the number of electrons
 initial_xvelocity = thermal_velocity.*cos(theta).*ones(e_num,1); % Sets the initial velocity as the thermal temperature  
@@ -52,7 +53,9 @@ new_yposition = initial_yposition;
 new_xvelocity = initial_xvelocity;
 new_yvelocity = initial_yvelocity;
 
-temp = [];
+initial_velocity = (mean(new_xvelocity.^2)) + (mean(new_yvelocity.^2));
+initial_temp = (initial_velocity*me)/(2*k);
+temp = [initial_temp];
 
 % Simulation loop that continually updates the simulation at each timestep
 % and calculates the 
@@ -60,6 +63,8 @@ for time = 1:simlength
         
 new_xposition = new_xposition + new_xvelocity*timestep;
 new_yposition = new_yposition + new_yvelocity*timestep;
+ 
+
 
 % Boundary Conditions being imposed 
 overboundx = new_xposition > 200e-9;
@@ -85,8 +90,8 @@ grid on
 axis([0 200e-9 0 100e-9]) 
 pause(sim_pause)
 
-averageVel = (mean(new_xvelocity.^2)) + (mean(new_yvelocity.^2));
-temp(time) = (averageVel*me)/(2*k);
+averageVel = (mean(new_xvelocity.^2)) + (mean(new_yvelocity.^2)); % Calculates the average velocity of the particles at given timestep
+temp(time) = (averageVel*me)/(2*k); % Using the avaerge velocity, find the temperature of the system at this giventimestep
 
 end
 hold off
@@ -105,26 +110,31 @@ pause(graph_pause)
 % Question 1.b MEAN FREE PATH
 % Mean Free Path Calculation 
 Tmn = 0.2e-12; % Mean time between collisions 
-MFP = averageVel*Tmn % Mean didtance travelled before collision occurs
+MFP_Q1 = averageVel*Tmn % Mean didtance travelled before collision occurs using the given mean time
 
 
 
 
 
-
-
+%------------------------------------------------------------------------------------------------------------------------------------------------
 % Question 2.a
+
 % Initial Calculations 
-% Question 1.a THERMAL VELOCITY
-thermal_velocity = sqrt((2*k*temperature)/me) % velocity in (m/s)
-std_thermal_velocity = 0.1*thermal_velocity;
-random_velocity = normrnd(thermal_velocity,std_thermal_velocity,[e_num,1]);
-bin_num = 7;
-figure(4)
+% The thermal velocity needs to be made random and assigned to each
+% variable. The random distribution is plotted in a histogram which shows
+% the randomly generated distribution
+thermal_velocity = sqrt((2*k*temperature)/me); % velocity in (m/s)
+distribution1 = randn(e_num,1)*(thermal_velocity/sqrt(2));
+distribution2 = randn(e_num,1)*(thermal_velocity/sqrt(2));
+maxwell_boltzmann_dist = sqrt((distribution1.^2)+(distribution2.^2));
+random_velocity = maxwell_boltzmann_dist;
+bin_num = 40;
+figure(8)
 velocity_hist = histogram(random_velocity,bin_num);
 title('Thermal Velocity Distribution')
 xlabel('Random Thermal Velocity (m/s)')
 ylabel('Number of Particles Within Range')
+grid on
 pause(graph_pause)
 
 % Initializing the Simulation Parameters 
@@ -150,19 +160,30 @@ new_yposition = initial_yposition;
 new_xvelocity = initial_xvelocity;
 new_yvelocity = initial_yvelocity;
 
-temp = [];
+initial_velocity = (mean(new_xvelocity.^2)) + (mean(new_yvelocity.^2));
+initial_temp = (initial_velocity*me)/(2*k);
+temp = [initial_temp];
+
+% Scattering Equations
+Pscatter = (1-exp(-(timestep/Tmn))); % Used to compare to random variable for scattering chance
+
+% Mean Free Path and Mean Collision Time Equations
+MFP = [];
+TMN = [];
 
 % Simulation loop that continually updates the simulation at each timestep
 % and calculates the 
 for time = 1:simlength 
 
 % Electron scattering and reevaluation of velocity
-rand_threshold = rand(e_num,1);
-Pscatter = (1-exp(-(timestep/Tmn)));
+rand_threshold = rand(e_num,1); % Sets a vector of random numbers for each electron
     for index = 1:e_num
         if rand_threshold(index) < Pscatter 
             theta = 2*pi*rand(1);
-            new_velocity = normrnd(thermal_velocity,std_thermal_velocity,[1,1]);
+            distribution1 = randn(1)*(thermal_velocity/sqrt(2));
+            distribution2 = randn(1)*(thermal_velocity/sqrt(2));
+            maxwell_boltzmann_dist = sqrt((distribution1.^2)+(distribution2.^2));
+            new_velocity = maxwell_boltzmann_dist;
             new_xvelocity(index) = cos(theta)*new_velocity;  
             new_yvelocity(index) = sin(theta)*new_velocity;
         end
@@ -194,16 +215,16 @@ pause(sim_pause)
 
 averageVel = (mean(new_xvelocity.^2)) + (mean(new_yvelocity.^2));
 temp(time) = (averageVel*me)/(2*k);
-
-
+MFP(time) = averageVel*Tmn; % Uses the given Tmn to find the MFP for this simulation
+TMN(time) = MFP_Q1/averageVel; % Uses the previously calculated MFP in part 1 to find the average time of collisions
 
 end
 hold off
 
+% Question 1.c.ii TEMPERATURE PLOT
 time = 1:simlength;
 
-% Question 1.c.ii TEMPERATURE PLOT
-figure(7) 
+figure(7) % Plotting the Temperature of the system
 plot(time*timestep,temp)
 title('Average Temperature Over Time')
 xlabel('Time (s)')
@@ -214,7 +235,8 @@ pause(graph_pause)
 % Question 1.b MEAN FREE PATH
 % Mean Free Path Calculation  
 Tmn = 0.2e-12; % Mean time between collisions 
-MFP = averageVel*Tmn % Mean didtance travelled before collision occurs
+MFP_average = mean(MFP) % Mean distance travelled before collision occurs
+TMN_average = mean(TMN) % Mean time between collisions calculated using a known 
 
 
 
@@ -228,95 +250,159 @@ MFP = averageVel*Tmn % Mean didtance travelled before collision occurs
 
 
 
+%------------------------------------------------------------------------------------------------------------------------------------------------
+% Question 3.a
+
+% Initial Calculations 
+% The thermal velocity needs to be made random and assigned to each
+% variable. The random distribution is plotted in a histogram which shows
+% the randomly generated distribution.
+thermal_velocity = sqrt((2*k*temperature)/me)       ; % velocity in (m/s)
+distribution1 = randn(e_num,1)*(thermal_velocity/sqrt(2));
+distribution2 = randn(e_num,1)*(thermal_velocity/sqrt(2));
+maxwell_boltzmann_dist = sqrt((distribution1.^2)+(distribution2.^2));
+random_velocity = maxwell_boltzmann_dist;
+bin_num = 40;
+figure(8)
+velocity_hist = histogram(random_velocity,bin_num);
+title('Thermal Velocity Distribution')
+xlabel('Random Thermal Velocity (m/s)')
+ylabel('Number of Particles Within Range')
+grid on
+pause(graph_pause)
+
+% Block Generation Code
 
 
+% Initializing the Simulation Parameters 
+initial_xposition = length*rand(e_num,1); % Sets the initial x positions as a vector of randomly selected numbers over the length of the simulation
+initial_yposition = height*rand(e_num,1); % Sets the initial y position as a vector of randomly selected numbers over the length of the simulation
+
+% Reinitializing electrons that spawned in restricted regions
+% Length of the channel or bottleneck and height are determined here
+Lbottle = 100e-9;
+Hbottle = 20e-9;
+% Checking the boundaries and reinitializing the ones that are restricted
+inboundx = (initial_xposition < ((length/2)+(Lbottle/2))) & (initial_xposition > ((length/2)-(Lbottle/2)));
+inboundy = (initial_yposition < ((height/2)-(Hbottle/2))) | (initial_yposition > ((height/2)+(Hbottle/2)));
+inbound = inboundx & inboundy;
+while(max(inbound) > 0)
+initial_xposition(inbound) = rand(size(initial_xposition(inbound),1),1)*length;
+initial_yposition(inbound) = rand(size(initial_yposition(inbound),1),1)*height;
+%initial_xposition(inbound) = rand(size(inbound,1),1)*length;
+%initial_yposition(inbound) = rand(size(inbound,1),1)*height;
+inboundx = (initial_xposition < ((length/2)+(Lbottle/2))) & (initial_xposition > ((length/2)-(Lbottle/2)));
+inboundy = (initial_yposition < ((height/2)-(Hbottle/2))) | (initial_yposition > ((height/2)+(Hbottle/2)));
+inbound = inboundx & inboundy;
+end
 
 
-% % Question 3.a 2D PLOT OF TRAJECTORIES 
-% 
-% % Bottleneck Characteristics
-% % These are all centred around the middle, the width is the size of the
-% % neck in x position and height fixes the hegith of the neck
-% bottle_width = 40e-9; % Remember that the max original width is 200-e9 m
-% bottle_height = 40e-9; % Remember the max original height is 100e-9 m
-% 
-% % Initializing the Simulation Parameters 
-% % No electrons start in the block regions 
-% initial_xposition = (length-bottle_width)*rand(e_num,1); % Sets the initial x positions as a vector of randomly selected numbers over the length of the simulation
-% initial_yposition = (height-bottle_height)*rand(e_num,1); % Sets the initial y position as a vector of randomly selected numbers over the length of the simulation
-% 
-% move_right = (initial_xposition > 0.5*(length-bottle_width)) & ((initial_yposition > 0.5*(height+bottle_height)) | (initial_yposition < 0.5*(height-bottle_height)))
-% initial_xposition(move_right) = initial_xposition(move_right) + bottle_width;
-% move_up = (initial_yposition > 0.5*(height-bottle_height))
-% initial_yposition(move_up) = initial_yposition(move_up) + bottle_height;
-% theta = 2*pi*rand(e_num,1); % Initializes a vector of random angles the size of the number of electrons
-% initial_xvelocity = thermal_velocity.*cos(theta).*ones(e_num,1); % Sets the initial velocity as the thermal temperature  
-% initial_yvelocity = thermal_velocity.*sin(theta).*ones(e_num,1); % 
-% 
-% figure(1)
-% plot(initial_xposition, initial_yposition, 'o')
-% title('Initial article Positions')
-% xlabel('X Position (m)')
-% ylabel('Y Position (m)')
-% axis([0 200e-9 0 100e-9])
-% grid on
-% 
-% timestep = 1e-15; % Timestep is the amount of time between each interval of the calculations 
-% 
-% new_xposition = initial_xposition; % Sets the 
-% new_yposition = initial_yposition;
-% new_xvelocity = initial_xvelocity;
-% new_yvelocity = initial_yvelocity;
-% 
-% temp = [];
-% 
-% % Simulation loop that continually updates the simulation at each timestep
-% % and calculates the 
-% for time = 1:simlength 
-%         
-% new_xposition = new_xposition + new_xvelocity*timestep;
-% new_yposition = new_yposition + new_yvelocity*timestep;
-% 
-% % Boundary Conditions being imposed 
-% overboundx = new_xposition > 200e-9;
-% underboundx = new_xposition < 0;
-% overboundy = new_yposition > 100e-9;
-% underboundy = new_yposition < 0;
-% new_xposition(overboundx) = new_xposition(overboundx) - 200e-9;  
-% new_xposition(underboundx) = new_xposition(underboundx) + 200e-9;
-% new_yvelocity(overboundy) = -new_yvelocity(overboundy);
-% new_yvelocity(underboundy) = -new_yvelocity(underboundy);
-% 
-% % Boundary Conditions for Bottleneck region
-% 
-% 
-% % Plotting the updating positions 
-% % Question 1.c.i
-% figure(2)
-% plot(new_xposition,new_yposition,'ro')
-% title('Simulation')
-% xlabel('Distance (nm)')
-% ylabel('Distance (nm)')
-% grid on
-% axis([0 200e-9 0 100e-9]) 
-% pause(0.01)
-% 
-% averageVel = (mean(new_xvelocity.^2)) + (mean(new_yvelocity.^2));
-% temp(time) = (averageVel*me)/(2*k);
-% 
-% end
-% hold off
-% 
-% time = 1:simlength;
-% 
-% % Question 1.c.ii
-% figure(3) 
-% plot(time,temp)
-% title('')
-% xlabel('')
-% ylabel('')
-% grid on
-% axis([time(1) time(end) 0 max(temp)+10])
+theta = 2*pi*rand(e_num,1); % Initializes a vector of random angles the size of the number of electrons
+initial_xvelocity = random_velocity.*cos(theta).*ones(e_num,1); % Sets the initial velocity as a value from the distribution that gives thermal velocity
+initial_yvelocity = random_velocity.*sin(theta).*ones(e_num,1); % 
+
+figure(9)
+plot(initial_xposition, initial_yposition, 'o')
+title('Initial Particle Positions')
+xlabel('X Position (m)')
+ylabel('Y Position (m)')
+axis([0 200e-9 0 100e-9])
+grid on
+pause(graph_pause)
+
+timestep = 1e-15; % Timestep is the amount of time between each interval of the calculations 
+
+new_xposition = initial_xposition; % Sets the 
+new_yposition = initial_yposition;
+new_xvelocity = initial_xvelocity;
+new_yvelocity = initial_yvelocity;
+
+initial_velocity = (mean(new_xvelocity.^2)) + (mean(new_yvelocity.^2));
+initial_temp = (initial_velocity*me)/(2*k);
+temp = [initial_temp];
+
+% Scattering Equations
+Pscatter = (1-exp(-(timestep/Tmn))); % Used to compare to random variable for scattering chance
+
+% Mean Free Path and Mean Collision Time Equations
+MFP = [];
+TMN = [];
+
+% Simulation loop that continually updates the simulation at each timestep
+% and calculates the 
+for time = 1:simlength 
+
+% Electron scattering and reevaluation of velocity
+rand_threshold = rand(e_num,1); % Sets a vector of random numbers for each electron
+    for index = 1:e_num
+        if rand_threshold(index) < Pscatter 
+            theta = 2*pi*rand(1);
+            distribution1 = randn(1)*(thermal_velocity/sqrt(2));
+            distribution2 = randn(1)*(thermal_velocity/sqrt(2));
+            maxwell_boltzmann_dist = sqrt((distribution1.^2)+(distribution2.^2));
+            new_velocity = maxwell_boltzmann_dist;
+            new_xvelocity(index) = cos(theta)*new_velocity;  
+            new_yvelocity(index) = sin(theta)*new_velocity;
+        end
+    end
+    
+new_xposition = new_xposition + new_xvelocity*timestep;
+new_yposition = new_yposition + new_yvelocity*timestep;
+
+% Boundary Conditions being imposed 
+overboundx = new_xposition > 200e-9;
+underboundx = new_xposition < 0;
+overboundy = new_yposition > 100e-9;
+underboundy = new_yposition < 0;
+new_xposition(overboundx) = new_xposition(overboundx) - 200e-9;  
+new_xposition(underboundx) = new_xposition(underboundx) + 200e-9;
+new_yvelocity(overboundy) = -new_yvelocity(overboundy);
+new_yvelocity(underboundy) = -new_yvelocity(underboundy);
+
+% if particles come from left and go over to restricted region, flip the
+% velocities
+overleft = ((new_xposition < (length/2)) & (new_xposition > ((length/2)-(Lbottle/2)))) & ((new_yposition < (height/2)-(Hbottle/2))) | (new_yposition > ((height/2)+(Hbottle/2))); 
+overright = ((new_xposition > (length/2)) & (new_xposition < ((length/2)+(Lbottle/2)))) & ((new_yposition < (height/2)-(Hbottle/2))) | (new_yposition > ((height/2)+(Hbottle/2)));
+
+new_xvelocity(overleft) = -new_xvelocity(overleft);
+new_xvelocity(overright) = -new_xvelocity(overright);
+
+
+% Plotting the updating positions 
+% Question 1.c.i 2D PLOT OF TRAJECTORIES
+figure(10)
+plot(new_xposition,new_yposition,'ro')
+title('Simulation')
+xlabel('Distance (nm)')
+ylabel('Distance (nm)')
+grid on
+axis([0 200e-9 0 100e-9]) 
+pause(sim_pause)
+
+averageVel = (mean(new_xvelocity.^2)) + (mean(new_yvelocity.^2));
+temp(time) = (averageVel*me)/(2*k);
+MFP(time) = averageVel*Tmn; % Uses the given Tmn to find the MFP for this simulation
+TMN(time) = MFP_Q1/averageVel; % Uses the previously calculated MFP in part 1 to find the average time of collisions
+
+end
+hold off
+
+% Question 1.c.ii TEMPERATURE PLOT
+time = 1:simlength;
+
+figure(11) % Plotting the Temperature of the system
+plot(time*timestep,temp)
+title('Average Temperature Over Time')
+xlabel('Time (s)')
+ylabel('Simulation Temperature (K)')
+grid on
+pause(graph_pause)
+
+% Question 1.b MEAN FREE PATH
+% Mean Free Path Calculation  
+Tmn = 0.2e-12; % Mean time between collisions 
+MFP_average = mean(MFP) % Mean distance travelled before collision occurs
+TMN_average = mean(TMN) % Mean time between collisions calculated using a known 
 
 
 
